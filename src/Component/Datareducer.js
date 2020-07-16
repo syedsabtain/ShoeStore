0a283c5e5cb2ffb17723d3b5f1ca4ea0c297fac6
syed <sabtain.info@gmail.com>
2020-07-16 11:18:16 -0700

    Reducer

---
+++ src/Component/Datareducer.js
@@ -0,0 +1,16 @@
+const Datareducer = (state,action)=>
+{
+    switch(action.type)
+    {
+        case 'ADD_ITEM':
+            return [...state,{name:action.payload.name,price:action.payload.price,id:action.payload.id}]
+        break;
+        case 'DELETE_ITEM':
+            return state.filter((val)=>val.id!=action.payload)
+        break;
+        default:
+            break;
+    }
+}
+
+export default  Datareducer
