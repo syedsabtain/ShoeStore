f4b3b42de3c4ce9c5c76d0b8415bc5b8ab0d74dd
syed <sabtain.info@gmail.com>
2020-07-16 11:16:50 -0700

    Shoes Component implementation of showing shows

---
+++ src/Shoes.js
@@ -0,0 +1,167 @@
+import adidas1 from './images/adidas-4d-run-1.0-shoes.jpg'
+import adidas2 from './images/busenitz-vulc-2.0-shoes.png'
+import adidas3 from './images/climacool-vento-shoes.jpg'
+import adidas4 from './images/continental-80-shoes.jpg'
+import adidas5 from './images/niteball-shoes.jpg'
+import adidas6 from './images/nizza-x-disney-sport-goofy-shoes.jpg'
+import adidas7 from './images/nmd_r1-shoes.jpg'
+import adidas8 from './images/nmd_r1-v2-shoes.jpg'
+import adidas9 from './images/primeblue-ultraboost-20-shoes.jpg'
+import adidas10 from './images/questar-flow-shoes.jpg'
+import adidas11 from './images/stan-smith-shoes.jpg'
+import adidas12 from './images/stan-smith-shoes.png'
+import adidas13 from './images/supernova-shoes.jpg'
+import adidas14 from './images/superstar-shoes.jpg'
+import adidas15 from './images/ultraboost-dna-tokyo-shoes.png'
+import adidas16 from './images/ultraboost-dna-x-disney-shoes.jpg'
+import adidas17 from './images/ultraboost-summer.rdy-shoes.jpg'
+import adidas18 from './images/x_plr-shoes.jpg'
+import adidas19 from './images/predator-mutator-20-plus-firm-ground-cleats.jpg'
+
+const imgshoe = [
+    adidas1,
+    adidas2,
+    adidas3,
+    adidas4,
+    adidas5,
+    adidas6,
+    adidas7,
+    adidas8,
+    adidas9,
+    adidas10,
+    adidas11,
+    adidas12,
+    adidas13,
+    adidas14,
+    adidas15,
+    adidas16,
+    adidas17,
+    adidas18,
+    adidas19
+]
+
+const detailshoes = [
+    'adidas-4d-run-1.0-shoes',
+    'busenitz-vulc-2.0-shoes',
+    'climacool-vento-shoes',
+    'continental-80-shoes',
+    'niteball-shoes',
+    'nizza-x-disney-sport-goofy-shoes',
+    'nmd_r1-shoes',
+    'nmd_r1-v2-shoes',
+    'primeblue-ultraboost-20-shoes',
+    'questar-flow-shoes',
+    'stan-smith-shoes',
+    'stan-smith-shoes',
+    'supernova-shoes',
+    'superstar-shoes',
+    'ultraboost-dna-tokyo-shoes',
+    'ultraboost-dna-x-disney-shoes',
+    'ultraboost-summer.rdy-shoes',
+    'x_plr-shoes',
+    'predator-mutator-20-plus-firm-ground-cleats'
+]
+
+const shoes = {
+
+  'adidas-4d-run-1.0-shoes': {
+        name:     'adidas-4d-run-1.0-shoes',
+        img: adidas1,
+        price:600
+    },
+    "busenitz-vulc-2.0-shoes": {
+        name:     'busenitz-vulc-2.0-shoes',
+        img: adidas2,
+        price:400
+    },
+    "climacool-vento-shoes": {
+        name:     'climacool-vento-shoes',
+        img: adidas3,
+        price:700
+    },
+    "continental-80-shoes": {
+        name:     'continental-80-shoes',
+        img: adidas4,
+        price:800
+    },
+    "niteball-shoes": {
+        name:     'niteball-shoes',
+        img: adidas5,
+        price:500
+    },
+    "nizza-x-disney-sport-goofy-shoes": {
+        name:     'nizza-x-disney-sport-goofy-shoes',
+        img: adidas6,
+        price:600
+    },
+    "nmd_r1-shoes": {
+        name:     'nmd_r1-shoes',
+        img: adidas7,
+        price:400
+    },
+    "nmd_r1-v2-shoes": {
+        name:     'nmd_r1-v2-shoes',
+        img: adidas8,
+        price:900
+    },
+    "primeblue-ultraboost-20-shoes": {
+        name:     'primeblue-ultraboost-20-shoes',
+        img: adidas9,
+        price:750
+    },
+    "questar-flow-shoes": {
+        name:     'questar-flow-shoes',
+        img: adidas10,
+        price:560
+    },
+    "stan-smith-shoes": {
+        name: 'stan-smith-shoes',
+        img: adidas11,
+        price:780
+    },
+    "stan-smith-shoes": {
+        name: 'stan-smith-shoes',
+        img: adidas12,
+        price:980
+    },
+    "supernova-shoes": {
+        name: 'supernova-shoes',
+        img: adidas13,
+        price:670
+    },
+    "superstar-shoes": {
+        name: 'superstar-shoes',
+        img: adidas14,
+        price:870
+    },
+    "ultraboost-dna-tokyo-shoes": {
+        name: 'ultraboost-dna-tokyo-shoes',
+        img: adidas15,
+        price:610
+    },
+    "ultraboost-dna-x-disney-shoes": {
+        name: 'ultraboost-dna-x-disney-shoes',
+        img: adidas16,
+        price:300
+    },
+    "ultraboost-summer.rdy-shoes": {
+        name: 'ultraboost-summer.rdy-shoes',
+        img: adidas17,
+        price:350
+    },
+    "x_plr-shoes": {
+        name: 'x_plr-shoes',
+        img: adidas18,
+        price:360
+    },
+    "predator-mutator-20-plus-firm-ground-cleats": {
+        name: 'predator-mutator-20-plus-firm-ground-cleats',
+        img: adidas19,
+        price:340
+    },
+}
+
+export default shoes;
+
+// 'fst':{   name:'adidas',   img:adidas1 }, 'snd':{   name:'adidas-3',
+// img:adidas2 }
